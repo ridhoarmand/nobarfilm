db4fec7d3f1423ca446a1a0d4fb91d27147efc19
Ridho Armansyah <ridhoarmand@yahoo.com>
2026-02-03 15:25:42 +0700

    fix: Error build socket

---
+++ src/types/api.js
@@ -0,0 +1,15 @@
+"use strict";
+// Sansekai MovieBox API Types// Based on documented API responses
+// ============================================
+// Common Types
+// ============================================
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SubjectType = void 0;
+var SubjectType;
+(function (SubjectType) {
+    SubjectType[SubjectType["Movie"] = 1] = "Movie";
+    SubjectType[SubjectType["Series"] = 2] = "Series";
+    SubjectType[SubjectType["Education"] = 5] = "Education";
+    SubjectType[SubjectType["Music"] = 6] = "Music";
+    SubjectType[SubjectType["Short"] = 7] = "Short";
+})(SubjectType || (exports.SubjectType = SubjectType = {}));
